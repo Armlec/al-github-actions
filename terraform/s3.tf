7174aacf93efa270eaf8a7b52f86b2f7a15df7ce
Armlec <alasdairmccabe@gmail.com>
2023-02-20 10:10:59 +0000

    Changed bucket to use environment name

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,9 @@
+variable "ENV_BUCKET_NAME" {
+    description = "Ingest lambda function"
+    type = string
+    default = "test-environment-name-"
+}
+
 resource "aws_s3_bucket" "test_bucket" {
-    bucket_prefix =  "alasdair-test-bucket"
+    bucket_prefix =  "${var.ENV_BUCKET_NAME}"
 }
