63f7ac4d20f1a0ffa8f83f4467614802f9d1334e
Armlec <alasdairmccabe@gmail.com>
2023-02-20 11:46:32 +0000

    Changed variable uasage to inside secret_string

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -4,16 +4,6 @@ variable "TOTESYS_DB_DATABASE" {}
 variable "TOTESYS_DB_USER" {}
 variable "TOTESYS_DB_PASSWORD" {}
 
-variable "TOTESYS_DB_CREDENTIALS" {
-    default = {
-        host: "${var.TOTESYS_DB_HOST}"
-        port: "${var.TOTESYS_DB_PORT}"
-        database: "${var.TOTESYS_DB_DATABASE}"
-        user: "${var.TOTESYS_DB_USER}"
-        password: "${var.TOTESYS_DB_PASSWORD}"
-                
-    }
-}
 
 resource "aws_secretsmanager_secret" "totesys-db-credentials" {
     name        = "totesys-db-credentials"
@@ -21,5 +11,5 @@ resource "aws_secretsmanager_secret" "totesys-db-credentials" {
 
 resource "aws_secretsmanager_secret_version" "example" {
     secret_id     = aws_secretsmanager_secret.totesys-db-credentials.id
-    secret_string = jsonencode(var.TOTESYS_DB_CREDENTIALS)
+    secret_string = "jsonencode({ host: ${var.TOTESYS_DB_HOST}, port: ${var.TOTESYS_DB_PORT}, database: ${var.TOTESYS_DB_DATABASE}, user: ${var.TOTESYS_DB_USER}, password: ${var.TOTESYS_DB_PASSWORD} })"
 }
