499c671c40b00469244f66b5ffb8d29b5efb9104
Armlec <alasdairmccabe@gmail.com>
2023-02-20 12:00:19 +0000

    changed jsonencode, creating new secret

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -4,12 +4,17 @@ variable "TOTESYS_DB_DATABASE" {}
 variable "TOTESYS_DB_USER" {}
 variable "TOTESYS_DB_PASSWORD" {}
 
-
-resource "aws_secretsmanager_secret" "totesys-db-credentials-3" {
-    name        = "totesys-db-credentials-3"
+resource "aws_secretsmanager_secret" "totesys-db-credentials-4" {
+    name        = "totesys-db-credentials-4"
 }
 
 resource "aws_secretsmanager_secret_version" "example" {
-    secret_id     = aws_secretsmanager_secret.totesys-db-credentials-3.id
-    secret_string = jsonencode("{ host: ${var.TOTESYS_DB_HOST}, port: ${var.TOTESYS_DB_PORT}, database: ${var.TOTESYS_DB_DATABASE}, user: ${var.TOTESYS_DB_USER}, password: ${var.TOTESYS_DB_PASSWORD} }")
+    secret_id     = aws_secretsmanager_secret.totesys-db-credentials-4.id
+    secret_string = jsonencode({
+        host: "${var.TOTESYS_DB_HOST}"
+        port: "${var.TOTESYS_DB_PORT}"
+        database: "${var.TOTESYS_DB_DATABASE}"
+        user: "${var.TOTESYS_DB_USER}"
+        password: "${var.TOTESYS_DB_PASSWORD}"          
+    })
 }
