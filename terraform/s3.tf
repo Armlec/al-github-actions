000dd5298f777cea4f6b97fa71c38e6d2ed5659f
Armlec <alasdairmccabe@gmail.com>
2023-02-20 10:18:03 +0000

    Moved variable decleration from .tf to github secrets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,3 @@
-variable "ENV_BUCKET_NAME" {
-    description = "Ingest lambda function"
-    type = string
-    default = "test-environment-name-"
-}
 
 resource "aws_s3_bucket" "test_bucket" {
     bucket_prefix =  "${var.ENV_BUCKET_NAME}"
