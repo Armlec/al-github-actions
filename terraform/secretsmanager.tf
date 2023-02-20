4012ef2e93ca5603e1434f112efa6164a5ca7838
Armlec <alasdairmccabe@gmail.com>
2023-02-20 11:36:41 +0000

    updated secret to json format

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,4 +1,19 @@
-variable "TOTESYS_DB_CREDENTIALS" {}
+variable "TOTESYS_DB_HOST" {}
+variable "TOTESYS_DB_PORT" {}
+variable "TOTESYS_DB_DATABASE" {}
+variable "TOTESYS_DB_USER" {}
+variable "TOTESYS_DB_PASSWORD" {}
+
+variable "TOTESYS_DB_CREDENTIALS" {
+    default = {
+        host: "${var.TOTESYS_DB_HOST}"
+        port: "${var.TOTESYS_DB_PORT}"
+        database: "${var.TOTESYS_DB_DATABASE}"
+        user: "${var.TOTESYS_DB_USER}"
+        password: "${var.TOTESYS_DB_PASSWORD}"
+                
+    }
+}
 
 resource "aws_secretsmanager_secret" "totesys-db-credentials" {
     name        = "totesys-db-credentials"
